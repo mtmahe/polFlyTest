01409084ef22d4cbca09ac43ea732c697a5ee369
mtmahe <mtmahe@gmail.com>
2022-03-02 16:10:05 -0400

    JS snippet complete.

--- src/app.js
+++ src/app.js
@@ -17,12 +17,65 @@
  * @return {boolean} A boolean indicating whether input matches expected{Cost,Year}
  */
 function isValid (input, expectedCost, expectedOldestYear) {
-  // Your code here
+  let inputLen = input.length;
+  let totalCost = 0;
+  let oldestYear = 3000;
+
+  for (let i = 0; i < inputLen; i++) {
+    if (input[i].type != 'Trailer') {
+      if (input[i].year < oldestYear) oldestYear = input[i].year;
+      totalCost += input[i].value;
+    }
+  }
+
+  if (totalCost == expectedCost && oldestYear == expectedOldestYear) {
+    return true
+  }
+
+  return false
 }
 
 /**
  * Prompt: Implement a few tests to validate that your function works as expected
  */
 function testIsValid () {
-  // Your code here
+  // My assumptions: Vehicles[] will not be empty and will contain at least 1 Truck or Tractor
+
+  const first = [
+    {type: 'Truck', year: 1990, value: 500}
+  ]
+  
+  if (isValid(first, 700, 1985)) {
+    console.log("First test passed")
+  } else {
+    console.log("First test failed")
+  }
+
+  // Check if working with 2 valid vehilces
+  const second = [
+    {type: 'Truck', year: 1990, value: 500}, 
+    {type: 'Tractor', year: 1985, value: 200}
+  ]
+
+  if (isValid(first, 700, 1985)) {
+    console.log("Second test passed")
+  } else {
+    console.log("Second test failed")
+  }
+
+  // Does it ignore the trailer?
+  const third = [
+    {type: 'Truck', year: 1990, value: 500}, 
+    {type: 'Tractor', year: 1985, value: 200},
+    {type: 'Trailer', year: 1970, value: 1000}
+  ]
+
+  if (isValid(third, 700, 1985)) {
+    console.log("Third test passed")
+  } else {
+    console.log("Third test failed")
+  }
+  
 }
+
+testIsValid()
